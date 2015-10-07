34f5cd9a32e059b64ee3cf69800637e9ce260638
wqchen68 <wqchen68@users.noreply.github.com>
2015-10-07 10:24:08 +0800

    Update crawler711.R
    修正 花蓮卓溪鄉的bug

--- crawler711.R
+++ crawler711.R
@@ -5,9 +5,9 @@ library(RCurl)
 data = NULL
 html0 <- readLines('http://www.319papago.idv.tw/lifeinfo/7-11/7-11-00.html',warn=F,encoding = "UTF-8") #首頁
 pagetree <- htmlTreeParse(html0, useInternalNodes = TRUE, encoding='UTF-8')
-url.list <- xpathSApply(pagetree, "//table[@width='746']/tr/td/a", xmlAttrs)
+url.list <- xpathSApply(pagetree, "//table[@width='746']/tr/td/a", xmlAttrs) #解析html
 
-for (i in 1:length(url.list)){ #第一層：縣市
+for (i in 17:length(url.list)){ #第一層：縣市
   
   urli = paste0('http://www.319papago.idv.tw/lifeinfo/7-11/',url.list[i],"")
   htmli <- readLines(urli, warn=F, encoding = "UTF-8")
@@ -24,12 +24,11 @@ for (i in 1:length(url.list)){ #第一層：縣市
       row <- xpathSApply(pagetree, "//table[@width='728']/tr/td", xmlValue) #為了台東縣長濱鄉的bug
       row = row[5:8] #http://www.319papago.idv.tw/lifeinfo/7-11/7-11-962.html
     }
-    if (nchar(row[1]) != 1){ #偏遠地區，沒有門市：http://www.319papago.idv.tw/lifeinfo/7-11/7-11-211.html
+    if (nchar(row[2]) != 1){ #偏遠地區，沒有門市：http://www.319papago.idv.tw/lifeinfo/7-11/7-11-211.html
+                             #花蓮卓溪鄉的bug     http://www.319papago.idv.tw/lifeinfo/7-11/7-11-982.html
       data = rbind(data,t(matrix(row,4,)))
     }
   }
 }
-
-
 
 
